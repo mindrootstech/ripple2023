2d183db9611c2885bf885ee5139cdd01197432d5
mindrootstech <mgupta@mind-roots.com>
2023-02-28 19:24:58 +0530

    code pushed by manish

--- lib/dashBoard/bottom_sheets/AllActionSheet.dart
+++ lib/dashBoard/bottom_sheets/AllActionSheet.dart
@@ -20,81 +20,18 @@ class AllActionSheet{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 10),
-            Stack(
-              children: [
-                GestureDetector(
-                  onTap: (){
-                    Get.back();
-                  },
-                  child: Image.asset(CommonUi.setPngImage("close_icon"),height: 30,width: 30),
-                ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(Strings.textAllActions, style: CommonUi.customTextStyle(
-                    fontFamily: Fonts.bold, fontSize: 20,
-                  )
-                  ),
-                )
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              height: 1.5,
-              width: Get.width,
-              color: ColorRes.noProgressColor,
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 24,bottom:24 ),
-              height: 50,
-              width: Get.width,
-              decoration: CommonUi.getBorderRadius(5.0, Colors.white, borderColor: ColorRes.lightTextColor),
-              child: Container(
-                margin: const EdgeInsets.only(left: 15, right: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+           getHeaderPart(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    SvgPicture.asset(CommonUi.setSvgImage("search_icon"), color: ColorRes.colorBlack),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 10),
-                        height: 50,
-                        child:
-                        TextFormField(
-                          // controller: controller.searchEditTextController.value,
-                          onChanged: (value) {},
-                          onFieldSubmitted: (val) {},
-                          style: CommonUi.customTextStyle(fontFamily: Fonts.medium, fontSize :14),
-                          decoration: InputDecoration(
-                            hintText: Strings.textSearchActionBusiness,
-                            hintStyle: CommonUi.customTextStyle(fontFamily: Fonts.medium, fontSize :14,
-                                color:  ColorRes.onboardIndicatorNonActiveColor),
-                            border: InputBorder.none,
-                          ),
-                          textCapitalization: TextCapitalization.none,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4, bottom: 4),
-                      child: Container(
-                        height: 50,
-                        color: ColorRes.noProgressColor,
-                        width: 1,
-                      ),
-                    ),
-                    const SizedBox(width: 14),
-                    Image.asset(CommonUi.setPngImage("filter_icon"),height: 24,width: 24),
-                    const SizedBox(width: 14)
+                    getCategoryListView(),
+                    getActionsList(),
                   ],
                 ),
               ),
             ),
-
-            getCategoryListView(),
-
-            getActionsList(),
+            
           ],
         ),
       ),
@@ -137,7 +74,7 @@ class AllActionSheet{
                 return Container(
                   height: 120,
                   width: 160,
-                  margin: EdgeInsets.only(right: index == 9 ? CommonUi.marginLeftRight : 10, left: index == 0 ? CommonUi.marginLeftRight : 0),
+                  margin: const EdgeInsets.only(right:  10),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -181,109 +118,190 @@ class AllActionSheet{
 
   Widget getActionsList(){
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(Strings.textActions, style: CommonUi.customTextStyle(fontFamily: Fonts.semiBold, fontSize: 18),),
         const SizedBox(height: 16),
-        Expanded(
-          child: SizedBox(
-            width: Get.width,
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1/1.2,
-                  mainAxisSpacing: 15),
-              physics: const BouncingScrollPhysics(),
-              padding: EdgeInsets.zero,
-              scrollDirection: Axis.vertical,
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  margin:  EdgeInsets.only(left: CommonUi.marginLeftRight , right: CommonUi.marginLeftRight),
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                              width: 168,
-                              height: 200,
-                              child: ClipRRect(
-                                borderRadius: const BorderRadius.only(topLeft: Radius.circular(8),topRight: Radius.circular(8)),
-                                child: Image.network(
-                                  "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg",
-                                  fit: BoxFit.cover,
-                                  loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress){
-                                    if (loadingProgress == null) return child;
-                                    return  Center(
-                                      child: CircularProgressIndicator(
-                                        color: Colors.white70,
-                                        value: loadingProgress.expectedTotalBytes != null
-                                            ? loadingProgress.cumulativeBytesLoaded /
-                                            loadingProgress.expectedTotalBytes!
-                                            : null,
-                                      ),
-                                    );
-                                  },
-                                ),
+        SizedBox(
+          width: Get.width,
+          child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1/1.2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 15),
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.zero,
+            scrollDirection: Axis.vertical,
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                // margin:  EdgeInsets.only(left: CommonUi.marginLeftRight , right: CommonUi.marginLeftRight),
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            width: 168,
+                            height: 200,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(topLeft: Radius.circular(8),topRight: Radius.circular(8)),
+                              child: Image.network(
+                                "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg",
+                                fit: BoxFit.cover,
+                                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress){
+                                  if (loadingProgress == null) return child;
+                                  return  Center(
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white70,
+                                      value: loadingProgress.expectedTotalBytes != null
+                                          ? loadingProgress.cumulativeBytesLoaded /
+                                          loadingProgress.expectedTotalBytes!
+                                          : null,
+                                    ),
+                                  );
+                                },
                               ),
                             ),
                           ),
-                          Container(
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20))
-                            ),
-                            padding: const EdgeInsets.all(14),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Image.asset(CommonUi.setPngImage("test_company"), height: 16,width: 45),
-                                    Container(
-                                      padding:const EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
-                                      decoration: CommonUi.curvedBoxDecoration(backgroundColor: ColorRes.appColor),
-                                      child: Text("50 Pts",style: CommonUi.customTextStyle(fontFamily: Fonts.heavy,fontSize: 11),),
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(height: 8),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 0, right: 8),
-                                  child: Text("Advocate for the restoration of seagrass in our oceans.",textAlign: TextAlign.start,style: CommonUi.customTextStyle(fontFamily: Fonts.semiBold,fontSize: 15),),
-                                ),
-                                Container(
-                                  height: 1,
-                                  margin: const EdgeInsets.only(top: 9),
-                                  color: ColorRes.colorGray,
-                                ),
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20))
+                          ),
+                          padding: const EdgeInsets.all(14),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset(CommonUi.setPngImage("test_company"), height: 16,width: 45),
+                                  Container(
+                                    padding:const EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
+                                    decoration: CommonUi.curvedBoxDecoration(backgroundColor: ColorRes.appColor),
+                                    child: Text("50 Pts",style: CommonUi.customTextStyle(fontFamily: Fonts.heavy,fontSize: 11),),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 8),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 0, right: 8),
+                                child: Text("Advocate for the restoration of seagrass in our oceans.",textAlign: TextAlign.start,style: CommonUi.customTextStyle(fontFamily: Fonts.semiBold,fontSize: 15),),
+                              ),
+                              Container(
+                                height: 1,
+                                margin: const EdgeInsets.only(top: 9),
+                                color: ColorRes.colorGray,
+                              ),
 
-                                Container(
-                                    width: Get.width,
-                                    margin: const EdgeInsets.only(right: 5, top: 10),
-                                    child: Align(
-                                        alignment: Alignment.topRight,
-                                        child: Image.asset(CommonUi.setPngImage("three_dots_icon"), height: 5,width: 25))),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      Container(
-                        alignment: Alignment.topRight,
-                        padding: const EdgeInsets.only(right: 5,top: 5),
-                        child: Image.asset(CommonUi.setPngImage("heart"),height: 25,width: 25,),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+                              Container(
+                                  width: Get.width,
+                                  margin: const EdgeInsets.only(right: 5, top: 10),
+                                  child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: Image.asset(CommonUi.setPngImage("three_dots_icon"), height: 5,width: 25))),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.topRight,
+                      padding: const EdgeInsets.only(right: 5,top: 5),
+                      child: Image.asset(CommonUi.setPngImage("heart"),height: 25,width: 25,),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ],
     );
   }
+
+ Widget getHeaderPart() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 10),
+        Stack(
+          children: [
+            GestureDetector(
+              onTap: (){
+                Get.back();
+              },
+              child: Image.asset(CommonUi.setPngImage("close_icon"),height: 30,width: 30),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text(Strings.textAllActions, style: CommonUi.customTextStyle(
+                fontFamily: Fonts.bold, fontSize: 20,
+              )
+              ),
+            )
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          height: 1.5,
+          width: Get.width,
+          color: ColorRes.noProgressColor,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 24,bottom:24 ),
+          height: 50,
+          width: Get.width,
+          decoration: CommonUi.getBorderRadius(5.0, Colors.white, borderColor: ColorRes.lightTextColor),
+          child: Container(
+            margin: const EdgeInsets.only(left: 15, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(CommonUi.setSvgImage("search_icon"), color: ColorRes.colorBlack),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    height: 50,
+                    child:
+                    TextFormField(
+                      // controller: controller.searchEditTextController.value,
+                      onChanged: (value) {},
+                      onFieldSubmitted: (val) {},
+                      style: CommonUi.customTextStyle(fontFamily: Fonts.medium, fontSize :14),
+                      decoration: InputDecoration(
+                        hintText: Strings.textSearchActionBusiness,
+                        hintStyle: CommonUi.customTextStyle(fontFamily: Fonts.medium, fontSize :14,
+                            color:  ColorRes.onboardIndicatorNonActiveColor),
+                        border: InputBorder.none,
+                      ),
+                      textCapitalization: TextCapitalization.none,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4, bottom: 4),
+                  child: Container(
+                    height: 50,
+                    color: ColorRes.noProgressColor,
+                    width: 1,
+                  ),
+                ),
+                const SizedBox(width: 14),
+                Image.asset(CommonUi.setPngImage("filter_icon"),height: 24,width: 24),
+                const SizedBox(width: 14)
+              ],
+            ),
+          ),
+        ),
+
+      ],
+    );
+ }
 
 
 }
